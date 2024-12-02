6c19628d92ca876ed720940d61a362b16533de89
Ayoub <34210464+ayoubsaif@users.noreply.github.com>
2024-12-02 08:31:59 +0100

    feat: add publisher_disable addon to remove telemetry to odoo

---
+++ addons/publisher_disable/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Publisher Disable',
+    'category': 'Productivity/Discuss',
+    'depends': ['mail'],
+    'description': """
+Module disables Publisher communitication with odoo
+=====================================
+
+This module disables message publisher of database information
+with Odoo to be private database no shared telemetry with odoo
+to respect European LOPD
+""",
+    'auto_install': True,
+    'license': 'OEEL-1'
+}
